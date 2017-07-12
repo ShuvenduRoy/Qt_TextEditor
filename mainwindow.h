4b380e59af8604d850c618c981548f1ce082dd1d
Bikash <bikash11roy@gmail.com>
2017-07-12 20:05:00 +0600

    Function added to headers

--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,25 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionSave_As_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
